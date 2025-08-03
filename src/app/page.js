352da6898d9a13fb97075f8553afb0443c67129c
dhruvvaidya835 <159634075+dhruvvaidya835@users.noreply.github.com>
2025-08-03 23:17:16 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,8 @@ import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const router = useRouter();
-
   useEffect(() => {
     router.push('/login');
   }, []);
-
   return null;
 }
