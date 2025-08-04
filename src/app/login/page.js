d13658e849970c096136c1dddde33e6d73f29548
dhruvvaidya835 <159634075+dhruvvaidya835@users.noreply.github.com>
2025-08-04 16:43:49 +0530

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -9,7 +9,7 @@ export default function LoginPage() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch('https://server-2-o4w0.onrender.com', {
+    const res = await fetch('https://server-3-v318.onrender.com', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
